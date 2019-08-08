4e4b2231dcdfd2a769b041e4b45a59d10d61ebf5
s_muxibillayev <sh.muhibillayev@mail.ru>
2019-08-08 16:37:01 +0500

    Add project files.

---
+++ tt.uz/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using tt.uz.Dtos;
+using tt.uz.Entities;
+
+namespace tt.uz.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+}
