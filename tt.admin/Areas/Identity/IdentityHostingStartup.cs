bffe466b1014193756047c16ac4b58cc67cc6e8c
s_muxibillayev <sh.muhibillayev@mail.ru>
2019-11-04 11:00:41 +0500

    Admin project Added

---
+++ tt.admin/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using tt.admin.Data;
+
+[assembly: HostingStartup(typeof(tt.admin.Areas.Identity.IdentityHostingStartup))]
+namespace tt.admin.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
