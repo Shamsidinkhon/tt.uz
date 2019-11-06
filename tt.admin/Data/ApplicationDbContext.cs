a6e503399745957e3c87e749d3011be44da49129
s_muxibillayev <sh.muhibillayev@mail.ru>
2019-11-06 09:09:10 +0500

    Admin News implemented

--- tt.admin/Data/ApplicationDbContext.cs
+++ tt.admin/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using tt.uz.Entities;
 
 namespace tt.admin.Data
 {
@@ -12,5 +13,12 @@ namespace tt.admin.Data
             : base(options)
         {
         }
+        public DbSet<tt.uz.Entities.News> News { get; set; }
+        public DbSet<tt.uz.Entities.Category> Categories { get; set; }
+        public DbSet<tt.uz.Entities.Price> Prices { get; set; }
+        public DbSet<tt.uz.Entities.Location> Locations { get; set; }
+        public DbSet<tt.uz.Entities.ContactDetail> ContactDetails { get; set; }
+        public DbSet<tt.uz.Entities.Image> Images { get; set; }
+
     }
 }
